5169e3ab082c9c1ed7a9cb4df7cecdab74e13101
Andrew <phongnguyentuan20@gmail.com>
2021-09-06 18:02:24 -0400

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { ErrorScreen } from '@ui/ErrorScreen';
+import React from 'react';
+
+const Custom404Page = () => {
+  return <ErrorScreen error="Page not found" />;
+};
+
+export default Custom404Page;
