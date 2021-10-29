a624147ab6416a825ffea05c7aebc57abeccb3d5
Vivy24 <62721809+Vivy24@users.noreply.github.com>
2021-10-29 13:44:41 -0400

    add approve posts api (#5)

---
+++ src/modules/approval-course-reviews/approval-service.ts
@@ -0,0 +1,9 @@
+import { firestore } from "@lib/firebase/firebase";
+
+const collectionRef = firestore.collection("course_reviews");
+
+export class ApprovalService {
+  static async approveReviewByID(reviewId: string) {
+    await collectionRef.doc(reviewId).update({ _isApproved: true });
+  }
+}
